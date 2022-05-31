b86ed0d427b0fd37b91bd108b3cf9f4a99c4fbe5
Murilo <muriloflimaa@gmail.com>
2022-05-31 14:58:30 -0300

    adicao de meta

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -7,6 +7,7 @@ const Home: NextPage = () => {
    <html lang='pt-BR'>
       <Head>
         <title>Assinatura | BuyPhone</title>
+        <meta name="description" content="Assine Buy Phone"></meta>
         <link rel="icon" type='image/ico' href="/favicon.ico" />
       </Head>
       </html>
